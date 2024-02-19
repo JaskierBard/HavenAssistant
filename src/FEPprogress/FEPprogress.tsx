3b7df56abd7e6e6b9df908e80c9c4973e3783f58
JaskierBared <mateuszswiderski009@gmail.com>
2024-02-19 13:12:13 +0100

    color FEP on Nav

--- src/FEPprogress/FEPprogress.tsx
+++ src/FEPprogress/FEPprogress.tsx
@@ -2,36 +2,14 @@ import { createDiv } from "../functions/createDiv";
 import "./FEPprogress.css";
 import { useEffect, useState } from "react";
 
-// const barProgress = {
-//   strength1: 0,
-//   agility1: 0,
-//   intelligence1: 0,
-//   constitution1: 0,
-//   perception1: 0,
-//   charisma1: 0,
-//   dexterity1: 0,
-//   will1: 0,
-//   psyche1: 0,
-//   strength2: 0,
-//   agility2: 0,
-//   intelligence2: 0,
-//   constitution2: 0,
-//   perception2: 0,
-//   charisma2: 0,
-//   dexterity2: 0,
-//   will2: 0,
-//   psyche2: 0,
-// };
 
 export const FEPprogress = (props: any) => {
   const [FEP, setFEP] = useState<number>(10);
 
   useEffect(() => {
-    // console.log(props.recievedFEP);
-
     if (props.recievedFEP) {
       props.recievedFEP.forEach((element: any) => {
-        console.log(element.value);
+        // console.log(element.value);
         createDiv(element, FEP);
       });
     }
--- src/functions/createDiv.tsx
+++ src/functions/createDiv.tsx
@@ -5,10 +5,9 @@ export const createDiv = (element: any, FEP:number) => {
       const childDiv = document.createElement("div");
       childDiv.style.height = `100%`;
       const barPercentage = ((element.value * 100) / FEP).toString();
+      console.log(`var(--${element.stat.split("+")[0]}1)`)
       childDiv.style.width = `${barPercentage}%`;
-      const cssName = element.stat.replace("+", "");
-      childDiv.className = cssName;
-
+      childDiv.style.backgroundColor = `var(--${element.stat.split("+")[0]}1)`;
       parentDiv.appendChild(childDiv);
     }
   };
