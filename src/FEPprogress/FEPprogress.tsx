241907c24d1b9665786e8cc3847c3b9945448520
JaskierBared <mateuszswiderski009@gmail.com>
2024-02-20 22:56:32 +0100

    divs creator show colorful infos about stats

--- src/FEPprogress/FEPprogress.tsx
+++ src/FEPprogress/FEPprogress.tsx
@@ -1,52 +1,72 @@
-import { createDiv } from "../functions/createDiv";
+import { FEPlimitChecker } from "../functions/FEPlimitChecker";
+import { createDiv, removeDivs } from "../functions/createDiv";
 import "./FEPprogress.css";
 import { useEffect, useState } from "react";
 
-
 export const FEPprogress = (props: any) => {
   const [FEP, setFEP] = useState<number>(10);
-
+  const [FEPtotal, setFEPtotal] = useState<{ stat: string; value: number }[]>(
+    []
+  );
   useEffect(() => {
-    if (props.recievedFEP) {
-      props.recievedFEP.forEach((element: any) => {
-        // console.log(element.value);
-        createDiv(element, FEP);
-      });
-    }
+    const processFEP = async () => {
+      const updatedFEPtotal = [...FEPtotal];
+
+      await Promise.all(
+        (props.recievedFEP || []).map(async (element: any) => {
+          const existingStatIndex = updatedFEPtotal.findIndex(
+            (stat) => stat.stat === element.stat
+          );
+
+          if (existingStatIndex !== -1) {
+            updatedFEPtotal[existingStatIndex].value += element.value;
+          } else {
+            updatedFEPtotal.push(element);
+          }
+        })
+      );
+      setFEPtotal(updatedFEPtotal);
+    };
+
+    processFEP();
   }, [props.recievedFEP]);
 
   useEffect(() => {
-    const myDivs = document.getElementsByClassName("FEPprogress");
-    Array.from(myDivs).forEach((myDiv) => {
-      while (myDiv.firstChild) {
-        myDiv.removeChild(myDiv.firstChild);
-      }
+    removeDivs();
+    let sum = 0;
+    FEPtotal?.forEach((element: any) => {
+      sum += element.value;
+
+      createDiv(element, FEP);
     });
-    if (props.recievedFEP) {
-      props.recievedFEP.forEach((element: any) => {
-        console.log(element.value);
-        createDiv(element, FEP);
-      });
-    }
-  }, [FEP]);
+    FEPlimitChecker(sum, FEP)
 
+    console.log(FEPtotal);
+  }, [FEPtotal]);
 
+  const clearFEP = () => {
+    setFEPtotal([]);
+    removeDivs();
+  };
 
   const handleInputChange = (e: any) => {
     setFEP(Number(e.target.value));
   };
 
   return (
-    <div>
+    <div className="FEPcontainer">
       <div className="FEPprogress"> </div>
+      <div className="FEPprogressInfo"></div>
 
       <input
+        className="FEPinput"
         type="text"
         id="yourInput"
         placeholder="FEP"
         value={FEP}
         onChange={handleInputChange}
       />
+      <button onClick={clearFEP}>Wyzeruj pasek</button>
     </div>
   );
 };
--- src/functions/createDiv.tsx
+++ src/functions/createDiv.tsx
@@ -1,13 +1,38 @@
-export const createDiv = (element: any, FEP:number) => {
-    const parentDiv = document.querySelector(".FEPprogress");
+export const createDiv = (element: any, FEP: number) => {
+  const parentDiv = document.querySelector(".FEPprogress");
 
-    if (parentDiv) {
-      const childDiv = document.createElement("div");
-      childDiv.style.height = `100%`;
-      const barPercentage = ((element.value * 100) / FEP).toString();
-      console.log(`var(--${element.stat.split("+")[0]}1)`)
-      childDiv.style.width = `${barPercentage}%`;
-      childDiv.style.backgroundColor = `var(--${element.stat.split("+")[0]}1)`;
-      parentDiv.appendChild(childDiv);
-    }
-  };
+  if (parentDiv) {
+    const childDiv = document.createElement("div");
+    childDiv.style.height = `100%`;
+    const barPercentage = ((element.value * 100) / FEP).toString();
+    childDiv.style.width = `${barPercentage}%`;
+    childDiv.style.backgroundColor = `var(--${element.stat.split("+")[0]}1)`;
+    childDiv.classList.add(`${element.stat},${element.value}`);
+    parentDiv.appendChild(childDiv);
+
+    childDiv.addEventListener("mouseover", () => {
+      const infoDiv = document.querySelector(".FEPprogressInfo");
+      if (infoDiv) {
+        infoDiv.innerHTML = `<p style=color:var(--${element.stat.split("+")[0]}1)>${element.stat} FEP:${element.value.toFixed(
+          2
+        )}</p>`;
+  
+      }
+    });
+
+    childDiv.addEventListener("mouseout", () => {
+      const infoDiv = document.querySelector(".FEPprogressInfo");
+      if (infoDiv) {
+        infoDiv.innerHTML = "";
+      }
+    });
+  }
+};
+
+export const removeDivs = () => {
+  const parentDiv = document.querySelector(".FEPprogress");
+
+  if (parentDiv) {
+    parentDiv.innerHTML = "";
+  }
+};
